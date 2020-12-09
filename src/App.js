8ad319606b7e716845d0da854dc04f366c738312
bhanukirant99 <bhanukirant99@gmail.com>
2020-12-10 01:22:39 +0530

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import QuestionCard from './QuestionCard'
-
+import Model from 'react-model'
+import { useState } from 'react'
 function App() {
+  const [isModelOpen, setIsModelOpen] = useState(false)
+
   return (
     <div className="app">
-      <QuestionCard />
+      <Model isOpen={isModelOpen} onRequestClose={() => setIsModelOpen(false)}>
+        <QuestionCard onClick={() => setIsModelOpen(true)}/>
+      </Model>
     </div>
   );
 }
--- src/QuestionCard.js
+++ src/QuestionCard.js
@@ -1,43 +1,61 @@
 import React from 'react'
 import './QuestionCard.css'
-import logo from './logo.svg';
 import { Avatar } from '@material-ui/core'
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 
-function QuestionCard() {
+function QuestionCard(reputation) {
+
     return (
-        <div className='questionCard'>
-            <div className="questionCard_top">
-                <Avatar className='questionCard_profilePicture' src='https://avatars.dicebear.com/4.5/api/human/1.svg'/>
-                <div className="questionCard_topInfo">
-                    <div className="questionCard_topTitle">
-                        Question Title
+        <div>
+                <div className='questionCard'>
+                    <div className="questionCard_top">
+                        {0 < reputation && reputation <= 50 ? (
+                                        <div>
+                                            <Avatar className='questionCard_profileRepuLow' src='https://avatars.dicebear.com/4.5/api/human/1.svg'/>
+                                        </div>
+                        ) : 50 < reputation && reputation <= 100 ? (
+                                    <div>
+                                        <Avatar className='questionCard_profileRepuMedium' src='https://avatars.dicebear.com/4.5/api/human/1.svg'/>
+                                    </div>
+                        ) : reputation > 100 ? (
+                                <div>
+                                    <Avatar className='questionCard_profileRepuHigh' src='https://avatars.dicebear.com/4.5/api/human/1.svg'/>
+                                </div>
+                        ) : (
+                            <div>
+                                <Avatar className='questionCard_profileRepuNor' src='https://avatars.dicebear.com/4.5/api/human/1.svg'/>
+                            </div>
+                        )}
+                        <div className="questionCard_topInfo">
+                            <div className="questionCard_topTitle">
+                                Question Title
+                            </div>
+                            <div className="questionCard_topQuestion">
+                                This is the Question
+                            </div>
+                        </div>
                     </div>
-                    <div className="questionCard_topQuestion">
-                        This is the Question
+                    <div className="questionCard_middle">
+                        <div className="questionCard_middleUserName">
+                            Username
+                        </div>
+                        <div className="questionCard_middleUpvotes">
+                            <ArrowUpwardIcon className='questionCard_middleUpvotesArrow'/>
+                            Upvotes
+                        </div>
+                    </div>
+                    <div className="questionCard_bottom">
+                        <div className="questionCard_bottomTag_1">
+                        Tag 1
+                        </div>
+                        <div className="questionCard_bottomTag_2">
+                            Tag 2
+                        </div>
+                        <div className="questionCard_bottomTag_3">
+                            Tag 3
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="questionCard_middle">
-                <div className="questionCard_middleUserName">
-                    Username
-                </div>
-                <div className="questionCard_middleUpvotes">
-                    <ArrowUpwardIcon />
-                    Upvotes
-                </div>
-            </div>
-            <div className="questionCard_bottom">
-                <div className="questionCard_bottomTag_1">
-                   Tag 1
-                </div>
-                <div className="questionCard_bottomTag_2">
-                    Tag 2
-                </div>
-                <div className="questionCard_bottomTag_3">
-                    Tag 3
-                </div>
-            </div>
         </div>
     )
 }
