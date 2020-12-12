f1bf2a80f8e63e1789378a5453fe8a7cc3ae7647
bhanukirant99 <bhanukirant99@gmail.com>
2020-12-12 23:38:54 +0530

    browser srashing due to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import QuestionCard from './components/QuestionCard'
 import Response from './response'
@@ -6,9 +6,36 @@ import { useStateValue } from './StateProvider';
 import InfiniteScroll from "react-infinite-scroll-component";
 
 function App() {
+  const data = Response
   const [input, setInput] = useState('');
   const [{items}, dispatch] = useStateValue()
+  const [currentItems, setCurrentItems]= useState([]);
+  const [hasMore, setHasMore]= useState([data.items]);
+  let pageNo = 1;
 
+  useEffect(() => {
+    while (pageNo) {
+      pageNo+=1;
+      fetch(`https://api.stackexchange.com/2.2/questions?order=desc&sort=hot&site=stackoverflow&page=${pageNo}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setCurrentItems({
+            items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+    }
+  }, []);
   // const addToBasket = () => {
   //     // console.log(basket)
   //     dispatch({
@@ -23,28 +50,63 @@ function App() {
   //     });
   // };
 
-  const data = Response
+  const fetchMoreData = () => {
+    if (currentItems.length >= 500) {
+      setHasMore(false)
+      return;
+    }
+    // a fake async api call like which sends
+    // 20 more records in .5 secs
+    setTimeout(() => {
+      setCurrentItems(currentItems.concat(Array.from({ length: 20 })))
+    }, 500);
+  };
+
   return (
     <div className="app">
       <div className="searchBar">
         <input className='searchBarInput' value={input} onChange={e => setInput(e.target.value)} placeholder='Search'/>
       </div>
       <div className="questionCards">
-        
-        {data.items?.map(item => (
-        <QuestionCard profilePicture=
-          {item.owner.profile_image} 
-          reputation={item.owner.reputation} 
-          userName={item.owner.display_name} 
-          title={item.title} 
-          upvotes={item.score} 
-          tags={item.tags} 
-          link={item.link}
-          views={item.view_count} 
-          askedAt={item.creation_date} 
-          answerCount={item.answer_count}
+        <InfiniteScroll
+          dataLength={items.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
         />
-      ))}
+        {/* {currentItems?.map(item => (
+          <QuestionCard profilePicture=
+            {item.owner.profile_image} 
+            reputation={item.owner.reputation} 
+            userName={item.owner.display_name} 
+            title={item.title} 
+            upvotes={item.score} 
+            tags={item.tags} 
+            link={item.link}
+            views={item.view_count} 
+            askedAt={item.creation_date} 
+            answerCount={item.answer_count}
+          />
+        ))} */}
+        {data.items?.map(item => (
+          <QuestionCard profilePicture=
+            {item.owner.profile_image} 
+            reputation={item.owner.reputation} 
+            userName={item.owner.display_name} 
+            title={item.title} 
+            upvotes={item.score} 
+            tags={item.tags} 
+            link={item.link}
+            views={item.view_count} 
+            askedAt={item.creation_date} 
+            answerCount={item.answer_count}
+          />
+        ))}
       </div>
     </div>
   );
