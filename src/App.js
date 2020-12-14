c8753c4354b7fbbcde2db9e611482c54a1a6a20f
bhanukirant99 <bhanukirant99@gmail.com>
2020-12-14 19:48:40 +0530

    adding the search feature

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ export default() => {
   const pageNo = 1
   const [input, setInput] = useState('');
   const [currentItems, setCurrentItems] = useState();
+  const [searchItems, setSearchItems] = useState([]);
   const [hasMore, setHasMore] = useState(true);
   const [currentPageNo, setCurrentPageNo] = useState(pageNo)
 
@@ -23,8 +24,6 @@ export default() => {
     setHasMore(resData.has_more)
   }, []);
 
-  console.log("currentitems: ", currentItems)
-
   const getMoreItems = async() => {
     const response = await fetch(`https://api.stackexchange.com/2.2/questions?order=desc&sort=hot&site=stackoverflow&page=${currentPageNo}`);
     const resData = await response.json();
@@ -35,14 +34,55 @@ export default() => {
     setHasMore(resData.has_more)
   }
 
+  const getSearchItems = () => {
+    setSearchItems([])
+    currentItems.map(item => {
+      const title = item.title;
+      const author = item.title;
+      const match = () => {
+        for (var i in title) {
+          for (var j in input) {
+            if (i === j) {
+              return true
+            }
+            else {
+              return false
+            }
+          }
+        }
+        for (var k in author) {
+          for (var l in input) {
+            if (k === l) {
+              return true
+            }
+            else {
+              return false
+            }
+          }
+        }
+      }
+      if (match) {
+        setSearchItems([...searchItems, item])
+      }
+    })
+    console.log("searchitems: ", searchItems)
+  }
+
   return (
     <div className="app">
       <div className="searchBar">
-        <input className='searchBarInput' value={input} onChange={e => setInput(e.target.value)} placeholder='Search'/>
+        <input className='searchBarInput' value={input} onChange={(e) => {
+            setInput(e.target.value)
+            if (input.length < 2 || input.length === 0) {
+              setSearchItems([])
+            } else {
+              getSearchItems()
+            }
+        }} placeholder='Search'/>
       </div>
       <div className="questionCards">
         <InfiniteScroll
-          dataLength={currentItems?.length || 0}
+          dataLength={searchItems.length || currentItems?.length || 0}
           next={getMoreItems}
           hasMore={hasMore}
           loader={<h2 style={{ textAlign: "center" }}>Loading...</h2>}
@@ -52,7 +92,40 @@ export default() => {
             </p>
           }
         >
-          {currentItems?.map(item => (
+          {
+            searchItems.length > 0 ? 
+            (searchItems?.map(item => (
+              <QuestionCard 
+                profilePicture={item.owner.profile_image} 
+                reputation={item.owner.reputation} 
+                userName={item.owner.display_name} 
+                title={item.title} 
+                upvotes={item.score} 
+                tags={item.tags} 
+                link={item.link}
+                views={item.view_count} 
+                askedAt={item.creation_date} 
+                isAnswered={item.is_answered}
+              />
+            )))
+            : (
+              currentItems?.map(item => (
+                <QuestionCard 
+                  profilePicture={item.owner.profile_image} 
+                  reputation={item.owner.reputation} 
+                  userName={item.owner.display_name} 
+                  title={item.title} 
+                  upvotes={item.score} 
+                  tags={item.tags} 
+                  link={item.link}
+                  views={item.view_count} 
+                  askedAt={item.creation_date} 
+                  isAnswered={item.is_answered}
+                />
+              ))
+            )
+          }
+          {/* {currentItems?.map(item => (
             <QuestionCard 
               profilePicture={item.owner.profile_image} 
               reputation={item.owner.reputation} 
@@ -65,7 +138,7 @@ export default() => {
               askedAt={item.creation_date} 
               isAnswered={item.is_answered}
             />
-          ))}
+          ))} */}
         </InfiniteScroll>
         {/* {currentItems && currentItems?.map(item => (
           <QuestionCard 
