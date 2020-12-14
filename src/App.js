c882d84a568e6f4f46ab55dca1e33eacf2e30a10
bhanukirant99 <bhanukirant99@gmail.com>
2020-12-14 18:33:55 +0530

    adding the dynamic infinite scroll

--- src/App.js
+++ src/App.js
@@ -2,117 +2,36 @@ import React, { useState, useEffect } from 'react'
 import './App.css';
 import QuestionCard from './components/QuestionCard'
 import Response from './response'
-// import { useStateValue } from './StateProvider';
 import InfiniteScroll from "react-infinite-scroll-component";
 
 export default() => {
   const data = Response
-  const items = 1
+  const pageNo = 1
   const [input, setInput] = useState('');
   const [currentItems, setCurrentItems] = useState();
   // const [hasMore, setHasMore] = useState(true);
-  const [NumberOfItems, setNumberOfItems] = useState(items)
-  // const [fetchedItems, setFetchedItems]= useState([]);
-  // const [getMore, setGetMore]= useState(true);
+  const [currentPageNo, setCurrentPageNo] = useState(pageNo)
 
   useEffect(async () => {
-    const response = await fetch(`https://api.stackexchange.com/2.2/questions?order=desc&sort=hot&site=stackoverflow&page=1`);
+    const response = await fetch(`https://api.stackexchange.com/2.2/questions?order=desc&sort=hot&site=stackoverflow&page=${currentPageNo}`);
     const resData = await response.json();
+    setCurrentPageNo(currentPageNo + 1)
     console.log(resData.itemd)
     // pageNo += 1;
     const [items] = resData.items
     setCurrentItems(resData.items)
-    // setCurrentItems([...currentItems, resData.items.map(item => {
-    //   return {
-    //     item  
-    //   }
-    // })])
-    // setCurrentItems(currentItems => currentItems.map(item => ({...item, {
-    //   tags: item.tags,
-    //   owner: item.owner,
-    //   is_answered: item.is_answered,
-    //   view_count: item.view_count,
-    //   score: item.score,
-    //   creation_date: item.creation_date,
-    //   link: item.link,
-    //   title: item.title,
-    // }})));
-    // resData.items.map(item => {
-    //   setCurrentItems([...currentItems, {
-    //     tags: item.tags,
-    //     owner: item.owner,
-    //     is_answered: item.is_answered,
-    //     view_count: item.view_count,
-    //     score: item.score,
-    //     creation_date: item.creation_date,
-    //     link: item.link,
-    //     title: item.title,
-    //   }])
-    // })
-    // resData.items.map(item => {
-    //     console.log( {
-    //       tags: item.tags,
-    //       owner: item.owner,
-    //       is_answered: item.is_answered,
-    //       view_count: item.view_count,
-    //       score: item.score,
-    //       creation_date: item.creation_date,
-    //       link: item.link,
-    //       title: item.title,
-    //     })
-    //   })
   }, []);
 
-  // while (hasMore) {
-  //   const getData = async() => {
-  //     const response = await fetch(`https://api.stackexchange.com/2.2/questions?order=desc&sort=hot&site=stackoverflow&page=${pageNo}`);
-  //     const resData = await response.json();
-  //     const [items] = await resData.items;
-  //     console.log(resData.items);
-  //     console.log(resData)
-  //     const success = await setCurrentItems(resData.items)
-  //     setHasMore(resData.has_more);
-  //     // setQuotaReamining(resData.quota_remaining);
-  //   }
-  //   pageNo += 1;
-  //   getData()
-  // }
-
   console.log("currentitems: ", currentItems)
-  
-//   const useFetch = (pageNo, getMore) => {
-//   const [fetchedItems, setFetchedItems]= useState([]);
-//   const [hasMore, setHasMore]= useState(false);
-//   // const [quotaReamining, setQuotaReamining] = useState(0);
 
-//   useEffect(async () => {
-//     const response = await fetch(`https://api.stackexchange.com/2.2/questions?order=desc&sort=hot&site=stackoverflow&page=${pageNo}`);
-//     const resData = await response.json();
-//     pageNo.value += 1;
-//     setFetchedItems(resData.items);
-//     setHasMore(resData.has_more);
-//     // setQuotaReamining(resData.quota_remaining);
-//   }, [getMore]);
-//   return {fetchedItems, hasMore}
-// }
-  
-  // const fetchMoreData = () => {
-  //   if (!hasMore) {
-  //     return;
-  //   }
-  //   if (currentItems.length >= 30) {
-  //     setHasMore(false);
-  //     return;
-  //   }
-  //   // a fake async api call like which sends
-  //   // 20 more records in .5 secs
-  //   setTimeout(() => {
-  //     setCurrentItems()
-  //     this.setState({
-  //       items: this.state.items.concat(Array.from({ length: 20 }))
-  //     });
-  //   }, 500);
-  // }
+  const getMoreItems = async() => {
+    const response = await fetch(`https://api.stackexchange.com/2.2/questions?order=desc&sort=hot&site=stackoverflow&page=${currentPageNo}`);
+    const resData = await response.json();
+    setCurrentPageNo(currentPageNo + 1)
+    resData.items.map(item => {
+      setCurrentItems([...currentItems, item])
+    })
+  }
 
   return (
     <div className="app">
@@ -122,9 +41,7 @@ export default() => {
       <div className="questionCards">
         <InfiniteScroll
           dataLength={currentItems?.length || 0}
-          next={() => (
-            setNumberOfItems(items+1)
-          )}
+          next={getMoreItems}
           hasMore={true}
           loader={<h4>Loading...</h4>}
           endMessage={
